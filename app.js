3d334d86ba7ea41ffec5fefc6c3b7bdc2011a063
Max Desktop <1@1.com>
2018-04-19 23:12:47 +0200

    progress

--- app.js
+++ app.js
@@ -1,16 +1,124 @@
+/** Express Setup **/
+
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fs = require('fs');
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res,next) {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+
+var wordsets = {};
+
+
+/** Emitter **/
+
+function sendNeueStats(){
+  for (var key in currentConnections){
+    currentConnections[key].socket.emit('neueStats', {guests: 4, rooms: 5, players: 4});
+  }
+  console.log('!! Neue Stats verteilt');
+}
+
+
+/** Receiver **/
+
+
+
+
+/** Auxillary **/
+
+function loadWordsets () {
+  var jsonData = fs.readFileSync("data/wordsets");
+  wordsets = JSON.parse(jsonData);
+}
+
+function saveWordsets () {
+  var jsonData = JSON.stringify(wordsets);
+  fs.writeFile("data/wordsets", jsonData, function(err) {
+      if (err) {
+          console.log(err);
+      }
+  });
+}
+
+function addSet(set) {
+  if (wordsets[set] == null) {
+    wordsets[set] = {};
+  }
+}
+
+/**
+function removeSet(set) {
+  var index = wordsets.indexOf(set);
+  if (index > -1) {
+      wordsets.splice(index, 1);
+  }
+}
+**/
+
+function addWord(set, word) {
+  if (wordsets[set].words == null) {
+    var temp = [word];
+    wordsets[set].words = temp;
+  } else {
+    if (wordsets[set].words.indexOf(word) == -1) {
+      wordsets[set].words.push(word);
+    }
+  }
+}
+
+function removeWord(set, word) {
+  var index = wordsets[set].words.indexOf(word);
+  if (index > -1) {
+      wordsets[set].words.splice(index, 1);
+  }
+}
+
+
+
+
+
+
+
+
+var currentConnections = {};
+
+io.on('connection', function (client) {
+  currentConnections[client.id] = {socket: client};
+  console.log('UserId: ' + client.id + ', UserName: ' + currentConnections[client.id].name + ' | Verbindung mit Server hergestellt');
+  sendNeueStats();
+
+  client.on('disconnect', function() {
+    delete currentConnections[client.id];
+  });
+
+
+  /** WORDSET KONFIG **/
+  client.on('askWordsetNames', function() {
+    var data = [];
+    for (var name in wordsets){
+      data.push(name);
+    }
+    client.socket.emit('giveWordsetNames',data);
+  });
+
+  client.on('askWordsetWords', function(set) {
+    client.socket.emit('giveWordsetWords',wordsets[set].words);
+  });
+
+
+  /**
+  client.on('data', function (somedata) {
+    currentConnections[client.id].data = someData;
+
+  });
+  **/
 });
 
 http.listen(3000);
--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,59 @@
+/** GLOBAL **/
 
-var element = function(id) {
+function element(id) {
   return document.getElementById(id);
 }
 
-var eingangspunkt = element('eingangspunkt');
-var spielerImRaum = document.createElement('ul');
-var spielfeld = document.createElement('ul');
+function clearElement(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
 
+var eingangspunkt = element('eingangspunkt');
+var buttons = element('footer-buttons');
+
+
+/** WELCOME **/
+
+function welcomeInit() {
+  clearElement(eingangspunkt);
+  clearElement(buttons);
+  createFooterBtn("Erstellen", "m3 left", "green lighten-2", spielraumInit);
+  createFooterBtn("Beitreten", "m3 left", "green lighten-2");
+  createFooterBtn("Konfiguration", "m3 right", "");
+
+
+  var root = document.createElement('div');
+  root.className = "container";
+  var card = document.createElement('div');
+  card.className = "card";
+  var cardimage = document.createElement('div');
+  cardimage.className = "card-image";
+  var img = document.createElement('img');
+  img.setAttribute("src", "img/banner.jpg");
+  cardimage.appendChild(img);
+  card.appendChild(cardimage);
+  root.appendChild(card);
+  eingangspunkt.appendChild(root);
+}
+
+
+/** SPIELRAUM **/
+
+var spielerImRaum = document.createElement('ul');
 spielerImRaum.id = "spielerImRaum";
 spielerImRaum.className = "col m3";
+var spielfeld = document.createElement('ul');
 spielfeld.id = "spielfeld";
 spielfeld.className = "row";
 
-function spielraumInit(eingangspunkt){
+function spielraumInit(){
+  clearElement(eingangspunkt);
+  clearElement(buttons);
+  clearElement(spielfeld);
+  createFooterBtn("Spiel verlassen", "m12", "green lighten-2", welcomeInit);
+
   var root = document.createElement('div');
   var temp = document.createElement('div');
   temp.className = "col m9";
@@ -25,26 +66,48 @@ function spielraumInit(eingangspunkt){
     var t1 = document.createElement('li');
     t1.className = "col m2";
     var t2 = document.createElement('div');
-    t2.className = "card-panel center valign-wrapper";
+    t2.className = "spielfeldKarte card-panel center valign-wrapper hoverable waves-effect waves-green";
     var t3 = document.createElement('span');
     t3.id = "Word" + i;
-    t3.appendChild(document.createTextNode("Word" + i));
+    var centerer = document.createElement('span');
+    centerer.className = "centerer";
+    t3.appendChild(document.createTextNode("Hund" + i));
+    t2.appendChild(centerer);
     t2.appendChild(t3);
     t1.appendChild(t2);
     spielfeld.appendChild(t1);
+
+    t2.addEventListener('click',function() {
+      this.classList.add('green', 'lighten-2');
+    });
   }
+  spielerlisteNeubau();
 }
 
-function spielerlisteNeubau (spielerImRaum){
-  for (var i = 0; i < 36; i++) {
+function spielerlisteNeubau (){
+  clearElement(spielerImRaum);
+
+  for (var i = 0; i < 5; i++) {
     var spielerCard = document.createElement('li');
     spielerCard.id = "player" + i;
     var t1 = document.createElement('div');
-    t1.className = "card";
+    t1.className = "card scale-transition";
     var t2 = document.createElement('div');
     t2.className = "card-content";
-    var t3 = document.createElement('p');
-    t3.appendChild(document.createTextNode("Hello World"));
+    var t4 = document.createElement('span');
+    t4.className = "card-title";
+    t4.appendChild(document.createTextNode("Tom"));
+
+    var t3 = document.createElement('ul');
+    t3.className = "row";
+    t3.id = "status" + i;
+    for (var j = 0; j < 36; j++) {
+      var tt1 = document.createElement('li');
+      tt1.className = "col m2 blue-grey";
+      t3.appendChild(tt1);
+    }
+
+    t2.appendChild(t4);
     t2.appendChild(t3);
     t1.appendChild(t2);
     spielerCard.appendChild(t1);
@@ -55,11 +118,17 @@ function spielerlisteNeubau (spielerImRaum){
 function spielerlisteAktualisieren(){}
 
 
-function actualiseStats(guests, rooms, players){
-  if (!guests || !rooms || !players) {
-    guests = 0;
-    rooms = 0;
-    players = 0;
+
+/** FOOTER **/
+
+function neueStats(data){
+  var guests = 0;
+  var rooms = 0;
+  var players = 0;
+  if (data) {
+    guests = data.guests;
+    rooms = data.rooms;
+    players = data.players;
   }
   if(guests < 2){
     element('numberOfGuests').textContent='| 1 Gast |';
@@ -70,17 +139,37 @@ function actualiseStats(guests, rooms, players){
   element('numberOfPlayers').textContent='| ' + players + ' Spieler |';
 }
 
+function createFooterBtn(text, alignement, buttonColor, callback) {
+  var root = document.createElement('span');
+  root.className = "col " + alignement;
+  var btn = document.createElement('a');
+  btn.className = "waves-effect waves-light btn " + buttonColor;
+  btn.appendChild(document.createTextNode(text));
+  root.appendChild(btn);
+  buttons.appendChild(root);
+
+  if (typeof callback === "function") {
+    btn.addEventListener('click', callback);
+  }
+}
+
+
+
+
+/** MAGIE AB HIER **/
+
 var socket = io.connect('http://localhost:3000');
 
 if(socket !== undefined){
-    console.log('Connected to socket...');
-    // Handle Output
-    actualiseStats();
-    spielraumInit(eingangspunkt);
-    spielerlisteNeubau(spielerImRaum);
-    socket.on('output', function(data){});
-    // Get Status From Server
-    socket.on('status', function(data){});
-    // Handle Input
-    socket.on('cleared', function(){});
+  welcomeInit();
+  //spielraumInit();
+  //spielerlisteNeubau(spielerImRaum);
+  socket.on('neueStats', data => {neueStats(data); console.log('yepa');});
+
+
+  socket.on('output', function(data){});
+  // Get Status From Server
+  socket.on('status', function(data){});
+  // Handle Input
+  socket.on('cleared', function(){});
 }
